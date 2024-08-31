ec94d3edb3fb7383bcc47946179cd27fe43e400d
Bárbara Pires <65234688+bahbi@users.noreply.github.com>
2024-08-31 18:27:55 -0300

    at

--- Dash_Visualizacao.py
+++ Dash_Visualizacao.py
@@ -7,9 +7,9 @@ import plotly.express as px
 st.title("Título aqui") 
 
 ## Visualização no streamlit
-aba1, aba2 = st.tabs(['Chamados', 'Sobre o clima'])
+aba1, aba2, aba3 = st.tabs(['Chamados', 'Chamados por evento','Sobre o clima'])
 
-#Dados
+#PARTE 1
 Chamados_1746_04_01 = pd.read_csv('./Dados_1/df_chamados_parte1.csv', delimiter=',')
 #df1 = pd.read_csv('/content/chamados_parte1.csv', delimiter=',')
 df_bairros = pd.read_csv('./Dados_1/bairros_rj.csv', delimiter=',')
@@ -36,24 +36,84 @@ aba1.dataframe(Chamados_1746_04_01)
 ocorrencias_por_tipo = Chamados_1746_04_01['tipo'].value_counts()
 df_ocorrencias_por_tipo = pd.DataFrame({'tipo': ocorrencias_por_tipo.index, 'contagem': ocorrencias_por_tipo.values})
 
-# Criando o gráfico de barras
-fig_tipo = px.bar(df_ocorrencias_por_tipo.head(10), x='contagem', y='tipo', orientation='h',
-                  labels={'contagem': 'Quantidade', 'tipo': 'Tipo de Ocorrência'},
-                  title='Top 10 Tipos de Ocorrências')
+#função
+def criar_grafico_barras(df, x='x', y='y', 
+                         top_n=10, orientation='h',
+                         titulo='Top Ocorrências',
+                         label_x='Quantidade', 
+                         label_y='', 
+                         paleta=px.colors.qualitative.Pastel2):
 
-# Personalizando as cores do gráfico
-fig_tipo.update_traces(marker_color='rgb(253, 180, 98)')
+    # Selecionar as primeiras top_n linhas
+    df_top = df.head(top_n)
+    
+    # Criar o gráfico de barras
+    fig = px.bar(
+        df_top,
+        x=x,  # Ajuste de acordo com a orientação
+        y=y,
+        orientation=orientation,
+        labels={x: label_x, y: label_y},
+        title=titulo,
+        color_discrete_sequence=paleta
+    )
+    
+    #labels={x: label_x, y: label_y}, labels={x: 'Quantidade', y: ''}, 
+    #fig_tipo.update_layout(yaxis={'categoryorder': 'total ascending'})
+
+    return fig
+
+
+# Exemplo de uso da função
+fig_tipo = criar_grafico_barras(df_ocorrencias_por_tipo,
+                                x='contagem', 
+                                y='tipo', 
+                                top_n=10, 
+                                orientation='h', 
+                                titulo='Top 10 Tipos de Ocorrências',
+                                label_x='Quantidade de Ocorrências',
+                                label_y='', 
+                                paleta=px.colors.qualitative.Pastel2)
+
+fig_tipo.update_layout(yaxis={'categoryorder': 'total ascending'})
 
 # Exibindo o gráfico de barras na aba1
-aba1.subheader("Chamados - Gráfico de Barras por Tipo de Ocorrência")
+aba1.subheader("Chamados - gráfico dos tipos")
 aba1.plotly_chart(fig_tipo)
+
+bairros_mais_chamados = df_uniao_chamados_bairros['nome'].value_counts()#.head(10)
+bairros_mais_chamados_df = pd.DataFrame({'nome':bairros_mais_chamados.index, 'contagem':bairros_mais_chamados.values})
+bairros_mais_chamados_df.head()
+
+
+
+fig_bairros = criar_grafico_barras(bairros_mais_chamados_df,
+                                x='contagem', 
+                                y='nome', 
+                                top_n=10, 
+                                orientation='h', 
+                                titulo='Top 10 Tipos de bairros',
+                                label_x='Quantidade de Ocorrências',
+                                label_y='', 
+                                paleta=px.colors.qualitative.Pastel2)
+
+fig_bairros.update_layout(yaxis={'categoryorder': 'total ascending'})
+
+# Exibindo o gráfico de barras na aba1
+aba1.subheader("Chamados - Gráfico de bairros")
+aba1.plotly_chart(fig_bairros)
 
 #------------------------------------------------------
 # Conteúdo da aba1 - exibição do gráfico
 aba1.subheader("Chamados - Gráfico de Linha")
 aba1.line_chart(Chamados_1746_04_01)
 
+
+#Parte 2
+
 aba2.subheader("A tab with the data") 
 #tab2.write(data)
 #st.dataframe(Chamados_1746_04_01)
 
+#Parte 3
+aba3.subheader("A tab with the data") 
